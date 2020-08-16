bf54c4acbe6bfea7a260264dd1f1bd67faec9c4c
Vinícius Gomes <vinigc19@gmail.com>
2020-08-16 11:29:59 -0300

    Movement random

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
-function test () {
-    alert('test');
+window.onload = animate();
+
+function newPosition () {
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+
+    return [Math.floor(Math.random() * height), Math.floor(Math.random() * width)];
+}
+
+function animate () {
+    var newq = newPosition();
+
+    document.getElementById('main').animate([
+        { transform: `translateX(${newq[0]}px)` },
+        { transform: `translateY(${newq[1]}px)` },
+    ],{
+        duration: 1000,
+        iterations: Infinity,
+
+      }
+    )
 }
